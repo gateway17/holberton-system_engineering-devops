0399e7a930f23ca3acd2693f5d0c3304b6d4143c
Andres Sepulveda <holberton@NNRX2000>
2021-09-17 21:26:04 -0500

    Usernames fixed.

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,12 +7,9 @@ if __name__ == "__main__":
     import json
     from sys import argv
 
-    # Who is the target?
-    TARGET_ID = argv[1]
     # PATHs that we need to make requests to
     PATHS = {
-        'Personal_D': "https://jsonplaceholder.typicode.com/users/{}"
-        .format(TARGET_ID),
+        'Personal_D': "https://jsonplaceholder.typicode.com/users/",
         'Personal_tasks': 'https://jsonplaceholder.typicode.com/todos/'
     }
     # Variables we will need
@@ -28,19 +25,22 @@ if __name__ == "__main__":
     # Name of json file
     FILE_NAME = 'todo_all_employees.json'
 
+    E_name = ''
+
+    u_buffer_id = {}
+
     # Make a GET request to get personal information about target
     with request.urlopen(PATHS['Personal_D']) as buffer:
         """Make a GET request to get personal information about target"""
-        data = buffer.read().decode('utf-8')
-        data = json.loads(data)
-        E_name = data['username']
+        U_data = buffer.read().decode('utf-8')
+        U_data = json.loads(U_data)
+
     # Get the whole list of tasks
     with request.urlopen(PATHS['Personal_tasks']) as buffer:
         """Get the whole list of tasks"""
         data = buffer.read().decode('utf-8')
         data = json.loads(data)
 
-    TARGET_ID = int(TARGET_ID)
     # Now, find how many of those tasks are for TARGET
 
     # get the whole list of users ID
@@ -54,8 +54,16 @@ if __name__ == "__main__":
         temp_list = []
         for a in data:
             if a['userId'] == e:
-                a = {'username': E_name, 'task': a['title'],
-                     'completed': a['completed']}
+ 
+                for i in U_data:
+                    if i['id'] == e:
+
+                        E_name = i['name']
+
+                        a = {'username': E_name, 'task': a['title'],
+                            'completed': a['completed']}
+                    else:
+                        continue
                 temp_list.append(a)
         e = str(e)
         final_dict[e] = temp_list
